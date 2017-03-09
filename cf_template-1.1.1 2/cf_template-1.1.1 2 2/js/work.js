00f1a8e42773009dd1d099556868ceeb47fe5ba6
tmurphy87 <tiffanymurphy73@gmail.com>
2017-03-09 16:45:10 -0600

    2.11

---
+++ cf_template-1.1.1 2/cf_template-1.1.1 2 2/js/work.js
@@ -0,0 +1,18 @@
+var works = [
+  {
+  	title: 'First Project',
+  	pic: 'img/cde2.jpg'
+  },
+  {
+  	title: 'Second Project',
+  	pic: 'img/cde3.jpg'
+  },
+  {
+  	title: 'Third Project',
+  	pic: 'img/wrk2.jpg'
+  },
+  {
+  	title: 'Fourth Project',
+  	pic: 'img/work3.jpg'
+  }
+];
